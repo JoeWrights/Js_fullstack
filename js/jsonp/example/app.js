14cd3dc31fc546503704c722fee36164b35b3f1c
JoeWrights <1035208578@qq.com>
2018-07-24 23:44:44 +0800

    jsonp学习

---
+++ js/jsonp/example/app.js
@@ -0,0 +1,19 @@
+const Koa = require('koa');
+const app = new Koa();
+
+const router = require('koa-router')();
+router.get('/api', (ctx) => {
+  const data = {
+    name: 'Joe',
+    age: 18
+  }
+  const callback = ctx.query.jsonpcallback;
+  if (callback) {
+    ctx.body = callback + '(' + JSON.stringify(data) + ')';
+  } else {
+    ctx.body = data;
+  }
+  // console.log(ctx.query);
+});
+app.use(router.routes());
+app.listen(3002);
